d689f7cbc05d690b003ba3db26e2827d38e402c9
Divs-Git <divyansh.sri258@gmail.com>
2024-11-30 16:18:45 +0530

    refactor into components

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,105 +1,32 @@
 <template>
   <div>
-    <header>
-      <h4>Question 1 / 3</h4>
-      <div class="bar">
-        <div class="completion"></div>
-      </div>
-    </header>
+    <QuizHeader :questionStatus="questionStatus" />
     <div>
-      <div class="questions-container">
-        <h1 class="question">What is the chemical value fo table salt</h1>
-      </div>
-      <div class="options-container">
-        <div class="option">
-          <p class="option-label">A</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-      </div>
-      <div class="options-container">
-        <div class="option">
-          <p class="option-label">A</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-      </div>
-      <div class="options-container">
-        <div class="option">
-          <p class="option-label">A</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-      </div>
-      <div class="options-container">
-        <div class="option">
-          <p class="option-label">A</p>
-          <div class="option-value">
-            <p>NaCl</p>
-          </div>
-        </div>
-      </div>
+      <Question :question="quiz.questions[currQuesIdx]" />
     </div>
   </div>
 </template>
-<script setup></script>
-<style scoped>
-header {
-  margin-top: 20px;
-}
 
-h4 {
-  font-size: 30px;
-}
+<script setup>
+import { useRoute } from 'vue-router'
+import Question from '../components/Question.vue'
+import QuizHeader from '../components/QuizHeader.vue'
+import { ref, watch } from 'vue'
+import quizes from '../data/quiz.json'
 
-.bar {
-  width: 300px;
-  height: 50px;
-  border: 3px solid bisque;
-}
+const route = useRoute()
+const quizID = parseInt(route.params.id)
+const currQuesIdx = ref(0)
 
-.completion {
-  height: 100%;
-  width: 0%;
-  background-color: bisque;
-}
+const quiz = quizes.find((q) => q.id === quizID)
 
-.questions-container {
-  margin-top: 20px;
-}
+const questionStatus = ref(`${currQuesIdx.value}/${quiz.questions.length}`)
+watch(
+  () => currQuesIdx.value,
+  () => {
+    questionStatus.value = `${currQuesIdx.value}/${quiz.questions.length}`
+  }
+)
+</script>
 
-.question {
-  font-size: 40px;
-  margin-bottom: 20px;
-}
-
-.option {
-  display: flex;
-  margin-bottom: 20px;
-  cursor: pointer;
-}
-
-.option-label {
-  background-color: bisque;
-  width: 50px;
-  height: 50px;
-  font-size: 30px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.option-value {
-  background-color: rgb(244, 239, 239);
-  width: 100%;
-  font-size: 30px;
-  padding: 0 20px;
-}
-
-.option-value p {
-  margin-top: 7px;
-}
-</style>
+<style scoped></style>
